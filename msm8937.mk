d0ba0b7c567b0906297cfde32d9f6d5394afb5b7
Rk779 <Rahulkrishna585@gmail.com>
2019-02-03 14:31:19 +0000

    msm8937-common: rootdir: start qseecomd on init
    Change-Id: I801e01634cf16fc57fcd2ebcc02fb41366fa8273

--- msm8937.mk
+++ msm8937.mk
@@ -273,8 +273,7 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sensors.sh \
     init.qcom.sh \
-    init.qcom.usb.sh \
-    init.qti.qseecomd.sh
+    init.qcom.usb.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
