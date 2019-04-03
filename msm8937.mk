9d76f9b2d5171d9e87fb2f39002b1771a72fa26d
Bowgo Tsai <bowgotsai@google.com>
2019-04-03 16:44:10 +0800

    msm8937-common: Adding GSI keys
    Bug: 112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1

--- msm8937.mk
+++ msm8937.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
