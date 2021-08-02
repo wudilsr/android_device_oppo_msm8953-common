4e9932a85f957286cfd7b4d5bd945bb75379a246
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 04:26:17 +0200

    msm8937-common: Build LED vibrator HIDL
    Change-Id: Ie758b8d7a5a3288dc9499cc7fbbd98a77632d102

--- msm8937.mk
+++ msm8937.mk
@@ -203,5 +203,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/oppo/msm8937-common/msm8937-common-vendor.mk)
