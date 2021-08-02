47544afb36eab84f1d3d0b80293a17459c6ff077
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 14:33:25 +0200

    msm8937-common: Import wifi configs
    * From ASUS_X00PD-user 10 QKQ1.191002.002 17.00.2003.23-20200401 release-keys
    
    Change-Id: If657d5dc4b09634d36dfff5483655ffa70cdc559

--- msm8937.mk
+++ msm8937.mk
@@ -314,6 +314,12 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 # Wifi Display
 PRODUCT_PACKAGES += \
     libnl
