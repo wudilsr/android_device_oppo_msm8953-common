291c1ad2dac3e4880eaf16d2ff9006481e7d1590
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 04:10:09 +0200

    msm8937-common: Build lights HIDL
    Change-Id: I7599102874691bd41ba71f29b7123c0b70a7f081

--- msm8937.mk
+++ msm8937.mk
@@ -119,6 +119,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcrypto.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    lights.msm8937
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
