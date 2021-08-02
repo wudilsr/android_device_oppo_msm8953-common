2436ad4b2676150a9399cd3c9a11700585cb98ed
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 03:43:03 +0200

    msm8937-common: Build audio stack
    Change-Id: If5cfae4a40883fe551b1ed72d51e36019c477eb0

--- msm8937.mk
+++ msm8937.mk
@@ -7,6 +7,34 @@
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl
+
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.primary.msm8937 \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libaudioroute \
+    libaacwrapper \
+    libaudio-resampler \
+    libaudiopreprocessing \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcompostprocbundle \
+    libtinycompress \
+    libvolumelistener \
+    tinymix
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
