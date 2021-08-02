dfe1b29a45d12895bc96615023bdca298829a8d1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 04:11:33 +0200

    msm8937-common: Build netutils-wrapper-1.0
    Change-Id: I52508aa65c0f438cfd8794fd3c4bb5b0c46cd80c

--- msm8937.mk
+++ msm8937.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8937
 
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
