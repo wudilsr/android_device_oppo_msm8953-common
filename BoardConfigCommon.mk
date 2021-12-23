xddeath69/device_asus_X00P-eleven

msm8937-common: Initial skeleton
Change-Id: I18e7ff5334ca76e9b9b0022cb429bf2316724adf

BoardConfigCommon.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/asus/msm8937-common
+
+# Inherit the proprietary files
+include vendor/asus/msm8937-common/BoardConfigVendor.mk

msm8937.mk
@@ -1,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/asus/msm8937-common/msm8937-common-vendor.mk)

wudilsr/android_device_oppo_msm8953-common

BoardConfigCommon.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/oppo/msm8937-common
+
+# Inherit the proprietary files
+include vendor/oppo/msm8937-common/BoardConfigVendor.mk

msm8937.mk
@@ -1,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/oppo/msm8937-common/msm8937-common-vendor.mk)
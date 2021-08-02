c880a1f727e980135286c95639c4041f7b9db00e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-02 02:15:53 +0200

    msm8937-common: Initial skeleton
    Change-Id: I18e7ff5334ca76e9b9b0022cb429bf2316724adf

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/oppo/msm8937-common
+
+# Inherit the proprietary files
+include vendor/oppo/msm8937-common/BoardConfigVendor.mk
