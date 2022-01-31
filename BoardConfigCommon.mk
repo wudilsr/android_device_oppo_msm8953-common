xddeath69/device_asus_X00P-eleven

msm8937-common: Add generic libinit
* Will just set Dalvik props
* Using ?= to be easily overridden

Change-Id: I3cecaf58db7a66938262b6947da98b3b536f0631

BoardConfigCommon.mk
@@ -85,6 +85,10 @@ ODM_MANIFEST_QCRILD_FILES := $(COMMON_PATH)/manifest_qcrild.xml
 # HW crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_asus_msm8937
+TARGET_RECOVERY_DEVICE_MODULES ?= init_asus_msm8937
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7

wudilsr/android_device_oppo_msm8953-common

BoardConfigCommon.mk
@@ -85,6 +85,10 @@ ODM_MANIFEST_QCRILD_FILES := $(COMMON_PATH)/manifest_qcrild.xml
 # HW crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
+# Init
+TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_oppo_msm8937
+TARGET_RECOVERY_DEVICE_MODULES ?= init_oppo_msm8937
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7