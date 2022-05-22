9c815f0bef30c7ec0b3631bb0b0cd9d3f6258a07
Ricky Cheung <rcheung844@gmail.com>
2022-05-22 16:27:03 +0800

    A57: Use stock sensor HALs
    Change-Id: I0715a75193256057fccad0205095b2203fceec36

--- rootdir/bin/init.qcom.sensors.sh
+++ rootdir/bin/init.qcom.sensors.sh
@@ -1,5 +1,5 @@
-#!/vendor/bin/sh
-# Copyright (c) 2015,2018 The Linux Foundation. All rights reserved.
+#!/system/bin/sh
+# Copyright (c) 2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -31,13 +31,14 @@
 #
 start_sensors()
 {
+    if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
+        chmod -h 775 /persist/sensors
+        chmod -h 664 /persist/sensors/sensors_settings
+        chown -h system.root /persist/sensors/sensors_settings
 
-    chmod -h 664 /persist/sensors/sensors_settings
-    chown -h -R system.system /persist/sensors
-    start vendor.sensors.qti
+        mkdir -p /data/misc/sensors
+        chmod -h 775 /data/misc/sensors
 
-    # Only for SLPI
-    if [ -c /dev/msm_dsps -o -c /dev/sensors ] ; then
         start vendor.sensors
     fi
 }
